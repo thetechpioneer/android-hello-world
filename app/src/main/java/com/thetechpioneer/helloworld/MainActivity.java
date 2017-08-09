31154bb326a0468af32d0c270ce55face48cba52
Tech Pioneer <thetechpioneer@gmail.com>
2017-08-09 18:36:00 +0530

    Basic hello world android example

---
+++ app/src/main/java/com/thetechpioneer/helloworld/MainActivity.java
@@ -0,0 +1,13 @@
+package com.thetechpioneer.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
